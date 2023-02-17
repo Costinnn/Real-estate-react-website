8e3a54e9e42c561488370853a63235937df6d308
Costinnn <crauciuc.costin@yahoo.com>
2023-02-17 18:31:29 +0200

    s4 design change

--- src/components/Home/Site.jsx
+++ src/components/Home/Site.jsx
@@ -4,6 +4,10 @@ import s1 from "../../assets/Home/s1.jpg";
 import s2 from "../../assets/Home/s2.jpeg";
 import s3 from "../../assets/Home/s3.jpg";
 import s4 from "../../assets/Home/s4.jpg";
+import no1 from "../../assets/Home/no1.png";
+import no2 from "../../assets/Home/no2.png";
+import no3 from "../../assets/Home/no3.png";
+import no4 from "../../assets/Home/no4.png";
 
 const Site = () => {
   return (
@@ -12,10 +16,38 @@ const Site = () => {
         <h1>Etape santier</h1>
         <div>
           <div className="photos">
-            <img src={s1} alt="" />
-            <img src={s2} alt="" />
-            <img src={s3} alt="" />
-            <img src={s4} alt="" />
+            <div className="box">
+              <img className="number" src={no1} alt="" />
+              <img src={s1} alt="" />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem et
+              vitae laudantium rerum voluptates.
+            </p>
+            <div className="box">
+              <img src={s2} alt="" />
+              <img className="number" src={no2} alt="" />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem et
+              vitae laudantium rerum voluptates.
+            </p>
+            <div className="box">
+              <img className="number" src={no3} alt="" />
+              <img src={s3} alt="" />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem et
+              vitae laudantium rerum voluptates.
+            </p>
+            <div className="box">
+              <img src={s4} alt="" />
+              <img className="number" src={no4} alt="" />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem et
+              vitae laudantium rerum voluptates.
+            </p>
           </div>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem et
